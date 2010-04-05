c1a8f5c07948c08ce6a0b9027f0f3f5243b12a01
beniz <beniz@hyperion.(none)>
2010-04-05 23:08:29 +0200

    added error call and response

--- src/dht/dht_err.h
+++ src/dht/dht_err.h
@@ -29,4 +29,6 @@ typedef int dht_err;
 #define DHT_ERR_UNKNOWN_PEER_LOCATION              6 /**< can't find a net address for a peer. */
 #define DHT_ERR_COM_TIMEOUT                        7 /**< a communication with a peer has timed out. */
 #define DHT_ERR_PTHREAD                            8 /**< thread creation error. */
+#define DHT_ERR_CALL                               9 /**< call error. */
+#define DHT_ERR_RESPONSE                          10 /**< response error. */
 #define DHT_ERR_UNKNOWN                         1000 /**< unknown error. */
