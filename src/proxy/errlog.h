ae2959004618b816f64cd252b31b5d071c424319
beniz <beniz@hyperion.(none)>
2010-04-09 20:27:59 +0200

    fixed errlog on DHT

--- src/proxy/errlog.h
+++ src/proxy/errlog.h
@@ -54,7 +54,7 @@
 #define LOG_LEVEL_CLF        0x0200 /* Common Log File format */
 #define LOG_LEVEL_CRUNCH     0x0400
 #define LOG_LEVEL_CGI        0x0800 /* CGI / templates */
-#define LOG_LEVEL_DHT        0x0300 /* DHT output */
+#define LOG_LEVEL_DHT        0x8000 /* DHT output */
 #define LOG_LEVEL_DHT_L1     0x0500 /* DHT stabilization. */
 
 /* Following are always on: */
