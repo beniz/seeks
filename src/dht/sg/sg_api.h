84b385412e1ea433a0c8b71ba7f9665c16c77eb4
beniz <beniz@phoebe.(none)>
2010-06-06 10:54:55 +0200

    added searchgroup API

---
+++ src/dht/sg/sg_api.h
@@ -0,0 +1,47 @@
+/**
+ * This is the p2p messaging component of the Seeks project,
+ * a collaborative websearch overlay network.
+ *
+ * Copyright (C) 2010  Emmanuel Benazera, juban@free.fr
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef SG_API_H
+#define SG_API_H
+
+#include "dht_err.h"
+#include "DHTKey.h"
+#include "Location.h"
+#include "subscriber.h"
+
+#include <vector>
+
+namespace dht
+{
+   
+   class sg_api
+     {
+      public:
+	static void get_sg_keys(const std::string &query, std::vector<DHTKey> &sg_keys,
+				const int &min_radius, const int &max_radius);	
+	
+	static dht_err find_sg(const DHTKey &sg_key, Location &node);
+	
+	static dht_err get_sg_peers(const DHTKey &sg_key, std::vector<Subscriber*> &peers);
+     };
+    
+} /* end of namespace. */
+
+#endif
