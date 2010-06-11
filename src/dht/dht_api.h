0c0b9745e92ecf173872791a9224e7bc2862e1da
beniz <beniz@hyperion.(none)>
2010-06-11 15:22:38 +0200

    added dht api

---
+++ src/dht/dht_api.h
@@ -0,0 +1,41 @@
+/**
+ * This is the p2p messaging component of the Seeks project,
+ * a collaborative websearch overlay network.
+ *
+ * Copyright (C) 2010  Emmanuel Benazera, juban@free.fr
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef DHT_API_H
+#define DHT_API_H
+
+#include "DHTNode.h"
+#include "DHTVirtualNode.h"
+
+namespace dht
+{
+   
+   class dht_api
+     {
+      public:
+	static dht_err findClosestPredecessor(const DHTNode &dnode,
+					      const DHTKey& nodeKey,
+					      DHTKey& dkres, NetAddress& na,
+					      int& status);
+     };
+      
+} /* end of namespace. */
+
+#endif
