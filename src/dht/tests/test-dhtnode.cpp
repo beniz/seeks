097536f4cb0e533d90fb31338828978f768da642
beniz <ebenazer@seeks-project.info>
2010-07-25 12:51:35 +0200

    added more options to DHT node test exe

--- src/dht/tests/test-dhtnode.cpp
+++ src/dht/tests/test-dhtnode.cpp
@@ -31,7 +31,7 @@ using namespace dht;
 using sp::miscutil;
 using sp::errlog;
 
-const char *usage = "Usage: test_dhtnode <ip:port> (--join ip:port) or (--self-bootstrap) (--persist)\n";
+const char *usage = "Usage: test_dhtnode <ip:port> (--join ip:port) or (--self-bootstrap) (--persist) (--config config_file)\n";
 
 bool persistence = false;
 DHTNode *dnode = NULL;
@@ -55,7 +55,7 @@ void sig_handler(int the_signal)
 
 int main(int argc, char **argv)
 {
-   if (argc < 2)
+   if (argc < 3)
      {
 	std::cout << usage;
 	exit(0);
@@ -71,30 +71,53 @@ int main(int argc, char **argv)
    errlog::init_log_module();
    errlog::set_debug_level(LOG_LEVEL_ERROR | LOG_LEVEL_DHT);
    
-   dnode = new DHTNode(net_addr,net_port);
-   
    bool joinb = false;
    bool sbootb = false;
    if (argc > 2)
      {
-	const char *joinopt = argv[2];
+	std::vector<NetAddress> bootstrap_nodelist;
+	int i=2;
+	while(i<argc)
+	  {
+	     const char *arg = argv[i];
+	     if (strcmp(arg,"--join")==0)
+	       {
+		  joinb = true;
+		  char *bootnode = argv[i+1];
+		  char* vec[2];
+		  miscutil::ssplit(bootnode,":",vec,SZ(vec),0,0);
+		  int bootstrap_port = atoi(vec[1]);
+		  std::cout << "bootstrap node at ip: " << vec[0] << " -- port: " << bootstrap_port << std::endl;
+		  NetAddress bootstrap_na(vec[0],bootstrap_port);
+		  bootstrap_nodelist.push_back(bootstrap_na);
+		  i+=2;
+	       }
+	     else if (strcmp(arg,"--self-bootstrap")==0)
+	       {
+		  sbootb = true;
+		  i++;
+	       }
+	     else if (strcmp(arg,"--persist") == 0)
+	       {
+		  persistence = true;
+		  i++;
+	       }
+	     else if (strcmp(arg,"--config") == 0)
+	       {
+		  DHTNode::_dht_config_filename = argv[i+1];
+		  std::cout << "dht config: " << DHTNode::_dht_config_filename << std::endl;
+		  i+=2;
+	       }
+	     else
+	       {
+		  std::cout << usage << std::endl;
+		  exit(0);
+	       }
+	  }
 	
-	if (strcmp(joinopt,"--join")==0)
-	  joinb = true;
-	else if (strcmp(joinopt,"--self-bootstrap")==0)
-	  sbootb = true;
-	else 
-	  {
-	     std::cout << usage;
-	     exit(0);
-	  }
-
-	if (argc > 3)
-	  {
-	     if (strcmp(argv[3],"--persist") == 0)
-	       persistence = true;
-	  }
-		
+	DHTNode::_dht_config = new dht_configuration(DHTNode::_dht_config_filename);
+	dnode = new DHTNode(net_addr,net_port);
+	
 	/**
 	 * unix signal handling for graceful termination.
 	 */
@@ -107,29 +130,18 @@ int main(int argc, char **argv)
 		    << catched_signals[idx] << std::endl;
 	       }
 	  }
-		
+	
 	/**
 	 * Join an existing ring or perform self-bootstrap.
 	 */
 	if (joinb)
 	  {
-	     char *bootnode = argv[3];
-	     char* vec[2];
-	     miscutil::ssplit(bootnode,":",vec,SZ(vec),0,0);
-	     
-	     int bootstrap_port = atoi(vec[1]);
-	     std::cout << "bootstrap node at ip: " << vec[0] << " -- port: " << bootstrap_port << std::endl;
-	     	     
-	     std::vector<NetAddress> bootstrap_nodelist;
-	     NetAddress bootstrap_na(vec[0],bootstrap_port);
-	     bootstrap_nodelist.push_back(bootstrap_na);
-	     
 	     bool reset = true;
 	     dnode->join_start(bootstrap_nodelist,reset);
 	  }
 	else if (sbootb)
 	  dnode->self_bootstrap();
      }
-   	
+   
    pthread_join(dnode->_l1_server->_rpc_server_thread,NULL);
 }
