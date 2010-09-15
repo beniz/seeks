34eb87b4a2fb824e51cbfba4662aa250ea47649b
beniz <ebenazer@seeks-project.info>
2010-09-15 21:21:36 +0200

    fixed bad merging

--- src/plugins/websearch/tests/test-dailymotion-parser.cpp
+++ src/plugins/websearch/tests/test-dailymotion-parser.cpp
@@ -42,11 +42,7 @@ int main(int argc, char **argv)
    long offset = 0;
    long length = -1;
    ifs.open(htmlpage,std::ios::binary);
-<<<<<<< HEAD:src/plugins/websearch/tests/test-dailymotion-parser.cpp
-   //ifs.open("dailymotion_markov_chain.html",std::ios::binary);
-=======
->>>>>>> c86f4b09a81132a5942279275236e0013d704bb9:src/plugins/websearch/tests/test-dailymotion-parser.cpp
-
+   
    if (!ifs.is_open())
      {
         std::cout << "[Error]: can't find file.\n";
@@ -70,12 +66,8 @@ int main(int argc, char **argv)
 
    se_parser_dailymotion spb;
    std::vector<search_snippet*> snippets;
-<<<<<<< HEAD:src/plugins/websearch/tests/test-dailymotion-parser.cpp
-   spb.parse_output(buffer,&snippets, 0);
-=======
    spb.parse_output_xml(buffer,&snippets, 0);
->>>>>>> c86f4b09a81132a5942279275236e0013d704bb9:src/plugins/websearch/tests/test-dailymotion-parser.cpp
-
+   
    std::cout << "snippets size: " << snippets.size() << std::endl;
 
    for (size_t i=0;i<snippets.size();i++)
