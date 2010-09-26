882d63f8fb9507bfb8beb430e86eca05dd1d5a8b
beniz <ebenazer@seeks-project.info>
2010-09-26 18:59:36 +0200

    missing uri_capture plugin files

---
+++ src/plugins/uri_capture/db_uri_record.cpp
@@ -0,0 +1,95 @@
+/**
+ * The Seeks proxy and plugin framework are part of the SEEKS project.
+ * Copyright (C) 2010 Emmanuel Benazera, ebenazer@seeks-project.info
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "db_uri_record.h"
+#include "db_uri_record_msg.pb.h"
+#include "errlog.h"
+
+#include <iostream>
+
+using sp::errlog;
+
+namespace seeks_plugins
+{
+   
+   db_uri_record::db_uri_record(const time_t &creation_time,
+				const std::string &plugin_name)
+     :db_record(creation_time,plugin_name),_hits(1)
+       {
+       }
+
+   db_uri_record::db_uri_record(const std::string &plugin_name)
+     :db_record(plugin_name),_hits(1)
+       {
+       }
+      
+   db_uri_record::db_uri_record()
+     :db_record(),_hits(0)
+       {
+       }
+   
+   db_uri_record::~db_uri_record()
+     {
+     }
+   
+   int db_uri_record::serialize(std::string &msg) const
+     {
+	sp::db::record r;
+	create_uri_record(r);
+	return r.SerializeToString(&msg);
+     }
+   
+   int db_uri_record::deserialize(const std::string &msg)
+     {
+	sp::db::record r;
+	if (!r.ParseFromString(msg))
+	  {
+	     errlog::log_error(LOG_LEVEL_ERROR,"failed deserializing db_uri_record");
+	     return 1; // error.
+	  }
+	read_uri_record(r);
+	return 0;
+     }
+
+   int db_uri_record::merge_with(const db_uri_record &dbr)
+     {
+	if (dbr._plugin_name != _plugin_name)
+	  return -2;
+	_hits += dbr._hits;
+	return 0;
+     }
+   
+   void db_uri_record::create_uri_record(sp::db::record &r) const
+     {
+	create_base_record(r);
+	r.SetExtension(sp::db::hits,_hits);
+     }
+      
+   void db_uri_record::read_uri_record(const sp::db::record &r)
+     {
+	read_base_record(r);
+	_hits = r.GetExtension(sp::db::hits);
+     }
+
+   std::ostream& db_uri_record::print(std::ostream &output)
+     {
+	output << "\thits: " << _hits << std::endl;
+	return output;
+     }
+   
+} /* end of namespace. */
---
+++ src/plugins/uri_capture/db_uri_record.h
@@ -0,0 +1,60 @@
+/**
+ * The Seeks proxy and plugin framework are part of the SEEKS project.
+ * Copyright (C) 2010 Emmanuel Benazera, ebenazer@seeks-project.info
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef DB_URI_RECORD_H
+#define DB_URI_RECORD_H
+
+#include "db_record.h"
+
+using sp::db_record;
+
+namespace seeks_plugins
+{
+   
+   class db_uri_record : public db_record
+     {
+      public:
+	db_uri_record(const time_t &creation_time,
+		      const std::string &plugin_name);
+	
+	db_uri_record(const std::string &plugin_name);
+	
+	db_uri_record();
+	
+	virtual ~db_uri_record();
+	
+	virtual int serialize(std::string &msg) const;
+	
+	virtual int deserialize(const std::string &msg);
+	
+	virtual int merge_with(const db_uri_record &dbr);
+	
+	void create_uri_record(sp::db::record &r) const;
+	
+	void read_uri_record(const sp::db::record &r);
+	
+	virtual std::ostream& print(std::ostream &output);
+	
+      public:
+	int _hits; /**< number of hits on this URI. */
+     };
+      
+} /* end of namespace. */
+
+#endif
+  
