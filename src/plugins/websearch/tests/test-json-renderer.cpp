fd8e29ffa189b7493196cc2a0de18bff3a87e67a
Loic Dachary <loic@dachary.org>
2010-10-21 09:52:58 +0200

    implement TEST(JsonRendererTest, render_snippets)

--- src/plugins/websearch/tests/test-json-renderer.cpp
+++ src/plugins/websearch/tests/test-json-renderer.cpp
@@ -25,7 +25,75 @@
 using namespace seeks_plugins;
 
 TEST(JsonRendererTest, render_engines) {
-  EXPECT_EQ("\"yahoo\"", json_renderer::render_engines(SE_YAHOO));
+  EXPECT_EQ("\"google\",\"bing\",\"yauba\",\"yahoo\",\"exalead\",\"twitter\"", json_renderer::render_engines(SE_GOOGLE|SE_BING|SE_YAUBA|SE_YAHOO|SE_EXALEAD|SE_TWITTER));
+}
+
+TEST(JsonRendererTest, render_snippets) {
+  websearch::_wconfig = new websearch_configuration("not a real filename");
+  std::string json_str;
+
+  int current_page = 1;
+  std::vector<search_snippet*> snippets;
+  const std::string query_clean;
+  hash_map<const char*, const char*, hash<const char*>, eqstr> parameters;
+
+  // zero snippet
+  ASSERT_EQ(SP_ERR_OK, json_renderer::render_snippets(query_clean, current_page, snippets, json_str, &parameters));
+  ASSERT_EQ("\"snippets\":[]", json_str);
+
+  // 1 snippet, page 1
+  json_str = "";
+  snippets.resize(1);
+  search_snippet s1;
+  s1.set_url("URL1");
+  snippets[0] = &s1;
+  ASSERT_EQ(SP_ERR_OK, json_renderer::render_snippets(query_clean, current_page, snippets, json_str, &parameters));
+  ASSERT_TRUE(json_str.find(s1._url) != std::string::npos);
+  ASSERT_EQ(std::string::npos, json_str.find("thumb"));
+  
+  // 1 snippet, page 1, thumbs on
+  json_str = "";
+  parameters.insert(std::pair<const char*,const char*>("thumbs", "on"));
+  ASSERT_EQ(SP_ERR_OK, json_renderer::render_snippets(query_clean, current_page, snippets, json_str, &parameters));
+  ASSERT_TRUE(json_str.find(s1._url) != std::string::npos);
+  ASSERT_NE(std::string::npos, json_str.find("thumb"));
+
+  // 3 snippets, page 2, 2 result per page, thumbs on
+  snippets.resize(3);
+  search_snippet s2;
+  s2.set_url("URL2");
+  snippets[1] = &s2;
+  search_snippet s3;
+  s3.set_url("URL3");
+  snippets[2] = &s3;
+  parameters.insert(std::pair<const char*,const char*>("rpp", "2"));
+
+  current_page = 2;
+  json_str = "";
+  ASSERT_EQ(SP_ERR_OK, json_renderer::render_snippets(query_clean, current_page, snippets, json_str, &parameters));
+  ASSERT_EQ(std::string::npos, json_str.find(s1._url));
+  ASSERT_EQ(std::string::npos, json_str.find(s2._url));
+  ASSERT_NE(std::string::npos, json_str.find(s3._url));
+
+  // 3 snippets, page 1, 2 result per page, thumbs on
+  current_page = 1;
+  json_str = "";
+  ASSERT_EQ(SP_ERR_OK, json_renderer::render_snippets(query_clean, current_page, snippets, json_str, &parameters));
+  ASSERT_NE(std::string::npos, json_str.find(s1._url));
+  ASSERT_NE(std::string::npos, json_str.find(s2._url));
+  ASSERT_EQ(std::string::npos, json_str.find(s3._url));
+
+  // 3 snippets, page 1, 2 result per page, similarity on, thumbs on
+  current_page = 1;
+  s1._seeks_ir = 1.0;
+  s2._seeks_ir = 0.0;
+  s3._seeks_ir = 1.0;
+  json_str = "";
+  ASSERT_EQ(SP_ERR_OK, json_renderer::render_snippets(query_clean, current_page, snippets, json_str, &parameters));
+  ASSERT_EQ("\"snippets\":[]", json_str);
+  ASSERT_EQ(std::string::npos, json_str.find(s1._url));
+  ASSERT_NE(std::string::npos, json_str.find(s2._url));
+  ASSERT_EQ(std::string::npos, json_str.find(s3._url));
 }
 
 int main(int argc, char **argv)
