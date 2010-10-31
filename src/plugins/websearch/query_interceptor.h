a6a408527caf3e91947f1195b13292b0649771c8
beniz <ebenazer@seeks-project.info>
2010-10-31 19:41:18 +0100

    fix to function signature

--- src/plugins/websearch/query_interceptor.h
+++ src/plugins/websearch/query_interceptor.h
@@ -1,6 +1,6 @@
 /**
  * The Seeks proxy and plugin framework are part of the SEEKS project.
- * Copyright (C) 2009 Emmanuel Benazera, juban@free.fr
+ * Copyright (C) 2009 Emmanuel Benazera, ebenazer@seeks-project.info
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
@@ -31,9 +31,9 @@ namespace seeks_plugins
 	query_interceptor(plugin *parent);
        
 	// virtual functions from plugin_element.
-	http_response* plugin_response(client_state *csp); // TODO.
+	http_response* plugin_response(client_state *csp);
 	
-	char* print() { return (char*)""; }; // TODO.
+	std::string print() { return ""; }; // TODO.
 	
 	// plugin functions.
 	hash_map<const char*,const char*,hash<const char*>,eqstr>* parse_query(http_request *http) const;
