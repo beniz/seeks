76605cafc5fdaf4a8442337ac9ebe7285f13fe1d
beniz <ebenazer@seeks-project.info>
2011-02-24 18:54:42 +0100

    missing query_capture error code header

---
+++ src/plugins/query_capture/qc_err.h
@@ -0,0 +1,29 @@
+/**
+ * The Seeks proxy and plugin framework are part of the SEEKS project.
+ * Copyright (C) 2011 Emmanuel Benazera, <ebenazer@seeks-project.info>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef QC_ERR_H
+#define QC_ERR_H
+
+#include "sp_err.h"
+typedef int qc_err;
+
+#define QC_ERR_STORE_QUERY           2001 /**< error while storing query. */
+#define QC_ERR_STORE_URL             2002 /**< error while storing URL. */
+#define QC_ERR_STORE                 2003 /**< generic storing error (URL + query). */
+
+#endif
