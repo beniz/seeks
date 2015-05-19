1ed372fc1ab845c5597d042fd851e0871ea73896
Roland Haeder <roland@mxchange.org>
2015-05-19 13:17:41 +0200

    Heavy improvements: - header('Location: bla-blub'); does *NOT* abort the script, *ALWAYS* use an explicit exit() call! - Check for $_SERVER array elements, not all browsers may send it causing E_NOTICE (bad code style) - Converted double-quotes to single-quotes - if() and else conditions are now in one line + curly braces are always explicitly set - Other small cleanups
    Signed-off-by: Roland Haeder <roland@mxchange.org>

--- resources/search.php
+++ resources/search.php
@@ -15,18 +15,40 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program. If not, see http://www.fsf.org/licensing/licenses/agpl-3.0.html. */
 
-if($_SERVER['HTTPS']) $scheme = 'https://';
-else $scheme= 'http://';
+// Default is HTTP
+$scheme = 'http://';
+
+if (isset($_SERVER['HTTPS'])) {
+	$scheme = 'https://';
+}
 
 $seeks_uri = 'http://s.s';
 $proxy = 'localhost:8250';
 $base_script = $_SERVER['SCRIPT_NAME'];
 $base_url = $scheme.$_SERVER['HTTP_HOST'].$base_script;
 
-if ($_SERVER['REQUEST_URI'] == '/search.php') { header('Location: '.$base_url.'/websearch-hp'); }
-else $url = $seeks_uri.str_replace($base_script, '', $_SERVER['REQUEST_URI']);
-$lang_head = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
-$referer = $_SERVER['HTTP_REFERER'];
+if ($_SERVER['REQUEST_URI'] == '/search.php') {
+	header('Location: '.$base_url.'/websearch-hp');
+
+	// Don't miss exit() as the server redirect doesn't abort the script
+	exit();
+} else {
+	$url = $seeks_uri . str_replace($base_script, '', $_SERVER['REQUEST_URI']);
+}
+
+// Avoid E_NOTICE by initializing variables and checking on array elements
+$lang_head = '';
+$referer = '';
+
+// Not all browsers are sending this header
+if (isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
+	$lang_head = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
+}
+
+// Not all browsers are sending this header
+if (isset($_SERVER['HTTP_REFERER'])) {
+	$referer = $_SERVER['HTTP_REFERER'];
+}
 
 $qc_redir = array();
 preg_match('/qc_redir/', $url, $qc_redir);
@@ -39,42 +61,46 @@ preg_match('/find_bqc/', $url, $bqc);
 
 $curl = curl_init();
 curl_setopt($curl, CURLOPT_URL, $url);
-if ($qc_redir[0] == "qc_redir"
-   || $tbd[0] == "tbd")
-{
- curl_setopt($curl, CURLOPT_HEADER, true);
+
+if ($qc_redir[0] == 'qc_redir' || $tbd[0] == 'tbd') {
+	curl_setopt($curl, CURLOPT_HEADER, true);
 }
+
 curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $_SERVER['REQUEST_METHOD']);
-if ($bqc[0] == "find_bqc")
-{
- $postdata = file_get_contents("php://input");
- curl_setopt($curl, CURLOPT_POST, 1);
- curl_setopt($curl, CURLOPT_POSTFIELDS, $postdata);
+
+if ($bqc[0] == 'find_bqc') {
+	$postdata = file_get_contents('php://input');
+	curl_setopt($curl, CURLOPT_POST, 1);
+	curl_setopt($curl, CURLOPT_POSTFIELDS, $postdata);
 }
+
 curl_setopt($curl, CURLOPT_PROXY, $proxy);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1) ;
-curl_setopt($curl, CURLOPT_HTTPHEADER, array("Seeks-Remote-Location: ".$base_url,"Accept-Language: ".$lang_head,"Referer: " .$referer));
+curl_setopt($curl, CURLOPT_HTTPHEADER, array('Seeks-Remote-Location: '.$base_url, 'Accept-Language: '.$lang_head, 'Referer: '.$referer));
 $result = curl_exec($curl);
 $result_info = curl_getinfo($curl);
-if(curl_errno($curl)) echo 'CURL ERROR: '.curl_error($curl);
+
+if(curl_errno($curl)) {
+	echo 'CURL ERROR: '.curl_error($curl);
+}
+
 curl_close($curl);
 
 header('Content-Type: '.$result_info['content_type']);
 
-if ($qc_redir[0] == "qc_redir"
-   || $tbd[0] == "tbd")
-{
- $status_code = array(); 
- preg_match('/\d\d\d/', $result, $status_code);
- switch( $status_code[0] ) {
-  case 302:
-  $location = array();
-  preg_match('/Location: (.*)/', $result, $location);
-  $location[0] = substr($location[0],10);
-  header("Location: ". $location[0]);
-  break; 
-  default:
- }
+if ($qc_redir[0] == 'qc_redir' || $tbd[0] == 'tbd') {
+	preg_match('/\d\d\d/', $result, $status_code);
+
+	switch( $status_code[0] ) {
+	case 302:
+		$location = array();
+		preg_match('/Location: (.*)/', $result, $location);
+		$location[0] = substr($location[0],10);
+		header('Location: '. $location[0]);
+
+		// Don't miss exit() as the server redirect doesn't abort the script
+		exit();
+	}
 }
 
 echo $result;
